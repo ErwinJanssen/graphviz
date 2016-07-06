055f6810a1df15e68551c6be53af18b762856bf3
Erwin Janssen <erwinjanssen@outlook.com>
2016-07-06 17:41:14 +0200

    Unit tests: Added dummy test case as a test.
    To see wether unit testing is working, I added a test that always returns true. This tests is executed during the make check step.

---
+++ cmd/dot/tests/parse_arguments_test.c
@@ -0,0 +1,15 @@
+/*
+ * parse_arguments_test.c
+ *
+ *  Created on: Jul 5, 2016
+ *      Author: erwin
+ */
+
+#include <criterion/criterion.h>
+#include "parse_arguments.h"
+
+
+Test(parse_argument, true_test)
+{
+	cr_assert(1== 1);
+}
