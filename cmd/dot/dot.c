5320d4c99eba32f4db46c0ff6bcf7a4481e57411
Erwin Janssen <erwinjanssen@outlook.com>
2016-07-21 12:21:07 +0200

    Autoformatted cmd/dot/dot.c
    Used the Eclipse autoformater with the built-in BSD/Allman style.

--- cmd/dot/dot.c
+++ cmd/dot/dot.c
@@ -26,8 +26,8 @@
 __declspec(dllimport) boolean MemTest;
 __declspec(dllimport) int GvExitOnUsage;
 /*gvc.lib cgraph.lib*/
-    #pragma comment( lib, "cgraph.lib" )
-    #pragma comment( lib, "gvc.lib" )
+#pragma comment( lib, "cgraph.lib" )
+#pragma comment( lib, "gvc.lib" )
 #else   /* not WIN32_DLL */
 #include "globals.h"
 #endif
@@ -57,59 +57,59 @@ static graph_t * G;
 #ifndef WIN32
 static void intr(int s)
 {
-/* if interrupted we try to produce a partial rendering before exiting */
-    if (G)
-	gvRenderJobs(Gvc, G);
-/* Note that we don't call gvFinalize() so that we don't start event-driven
- * devices like -Tgtk or -Txlib */
-    exit (gvFreeContext(Gvc));
+	/* if interrupted we try to produce a partial rendering before exiting */
+	if (G)
+		gvRenderJobs(Gvc, G);
+	/* Note that we don't call gvFinalize() so that we don't start event-driven
+	 * devices like -Tgtk or -Txlib */
+	exit(gvFreeContext(Gvc));
 }
 
 #ifndef NO_FPERR
 static void fperr(int s)
 {
-    fprintf(stderr, "caught SIGFPE %d\n", s);
-    /* signal (s, SIG_DFL); raise (s); */
-    exit(1);
+	fprintf(stderr, "caught SIGFPE %d\n", s);
+	/* signal (s, SIG_DFL); raise (s); */
+	exit(1);
 }
 
 static void fpinit(void)
 {
 #if defined(HAVE_FENV_H) && defined(HAVE_FEENABLEEXCEPT)
-    int exc = 0;
+	int exc = 0;
 # ifdef FE_DIVBYZERO
-    exc |= FE_DIVBYZERO;
+	exc |= FE_DIVBYZERO;
 # endif
 # ifdef FE_OVERFLOW
-    exc |= FE_OVERFLOW;
+	exc |= FE_OVERFLOW;
 # endif
 # ifdef FE_INVALID
-    exc |= FE_INVALID;
+	exc |= FE_INVALID;
 # endif
-    feenableexcept(exc);
+	feenableexcept(exc);
 
 #ifdef HAVE_FESETENV
 #ifdef FE_NONIEEE_ENV
-    fesetenv (FE_NONIEEE_ENV);
+	fesetenv (FE_NONIEEE_ENV);
 #endif
 #endif
 
 #elif  HAVE_FPU_CONTROL_H
-    /* On s390-ibm-linux, the header exists, but the definitions
-     * of the masks do not.  I assume this is temporary, but until
-     * there's a real implementation, it's probably safest to not
-     * adjust the FPU on this platform.
-     */
+	/* On s390-ibm-linux, the header exists, but the definitions
+	 * of the masks do not.  I assume this is temporary, but until
+	 * there's a real implementation, it's probably safest to not
+	 * adjust the FPU on this platform.
+	 */
 # if defined(_FPU_MASK_IM) && defined(_FPU_MASK_DM) && defined(_FPU_MASK_ZM) && defined(_FPU_GETCW)
-    fpu_control_t fpe_flags = 0;
-    _FPU_GETCW(fpe_flags);
-    fpe_flags &= ~_FPU_MASK_IM;	/* invalid operation */
-    fpe_flags &= ~_FPU_MASK_DM;	/* denormalized operand */
-    fpe_flags &= ~_FPU_MASK_ZM;	/* zero-divide */
-    /*fpe_flags &= ~_FPU_MASK_OM;        overflow */
-    /*fpe_flags &= ~_FPU_MASK_UM;        underflow */
-    /*fpe_flags &= ~_FPU_MASK_PM;        precision (inexact result) */
-    _FPU_SETCW(fpe_flags);
+	fpu_control_t fpe_flags = 0;
+	_FPU_GETCW(fpe_flags);
+	fpe_flags &= ~_FPU_MASK_IM; /* invalid operation */
+	fpe_flags &= ~_FPU_MASK_DM; /* denormalized operand */
+	fpe_flags &= ~_FPU_MASK_ZM; /* zero-divide */
+	/*fpe_flags &= ~_FPU_MASK_OM;        overflow */
+	/*fpe_flags &= ~_FPU_MASK_UM;        underflow */
+	/*fpe_flags &= ~_FPU_MASK_PM;        precision (inexact result) */
+	_FPU_SETCW(fpe_flags);
 # endif
 #endif
 }
@@ -120,83 +120,92 @@ static graph_t *create_test_graph(void)
 {
 #define NUMNODES 5
 
-    Agnode_t *node[NUMNODES];
-    Agedge_t *e;
-    Agraph_t *g;
-    Agraph_t *sg;
-    int j, k;
-    char name[10];
+	Agnode_t *node[NUMNODES];
+	Agedge_t *e;
+	Agraph_t *g;
+	Agraph_t *sg;
+	int j, k;
+	char name[10];
 
-    /* Create a new graph */
-    g = agopen("new_graph", Agdirected,NIL(Agdisc_t *));
+	/* Create a new graph */
+	g = agopen("new_graph", Agdirected, NIL(Agdisc_t *));
 
-    /* Add nodes */
-    for (j = 0; j < NUMNODES; j++) {
-	sprintf(name, "%d", j);
-	node[j] = agnode(g, name, 1);
-	agbindrec(node[j], "Agnodeinfo_t", sizeof(Agnodeinfo_t), TRUE);	//node custom data
-    }
-
-    /* Connect nodes */
-    for (j = 0; j < NUMNODES; j++) {
-	for (k = j + 1; k < NUMNODES; k++) {
-	    e = agedge(g, node[j], node[k], NULL, 1);
-	    agbindrec(e, "Agedgeinfo_t", sizeof(Agedgeinfo_t), TRUE);	//edge custom data
+	/* Add nodes */
+	for (j = 0; j < NUMNODES; j++)
+	{
+		sprintf(name, "%d", j);
+		node[j] = agnode(g, name, 1);
+		agbindrec(node[j], "Agnodeinfo_t", sizeof(Agnodeinfo_t), TRUE);	//node custom data
 	}
-    }
-    sg = agsubg (g, "cluster1", 1);
-    agsubnode (sg, node[0], 1);
 
-    return g;
+	/* Connect nodes */
+	for (j = 0; j < NUMNODES; j++)
+	{
+		for (k = j + 1; k < NUMNODES; k++)
+		{
+			e = agedge(g, node[j], node[k], NULL, 1);
+			agbindrec(e, "Agedgeinfo_t", sizeof(Agedgeinfo_t), TRUE);//edge custom data
+		}
+	}
+	sg = agsubg(g, "cluster1", 1);
+	agsubnode(sg, node[0], 1);
+
+	return g;
 }
 
 int main(int argc, char **argv)
 {
-    graph_t *prev = NULL;
-    int r, rc = 0;
+	graph_t *prev = NULL;
+	int r, rc = 0;
 
-    Gvc = gvContextPlugins(lt_preloaded_symbols, DEMAND_LOADING);
-    GvExitOnUsage = 1;
-    gvParseArgs(Gvc, argc, argv);
+	Gvc = gvContextPlugins(lt_preloaded_symbols, DEMAND_LOADING);
+	GvExitOnUsage = 1;
+	gvParseArgs(Gvc, argc, argv);
 #ifndef WIN32
-    signal(SIGUSR1, gvToggle);
-    signal(SIGINT, intr);
+	signal(SIGUSR1, gvToggle);
+	signal(SIGINT, intr);
 #ifndef NO_FPERR
-    fpinit();
-    signal(SIGFPE, fperr);
+	fpinit();
+	signal(SIGFPE, fperr);
 #endif
 #endif
 
-    if (MemTest) {
-	while (MemTest--) {
-	    /* Create a test graph */
-	    G = create_test_graph();
+	if (MemTest)
+	{
+		while (MemTest--)
+		{
+			/* Create a test graph */
+			G = create_test_graph();
 
-	    /* Perform layout and cleanup */
-	    gvLayoutJobs(Gvc, G);  /* take layout engine from command line */
-	    gvFreeLayout(Gvc, G);
-	    agclose (G);
+			/* Perform layout and cleanup */
+			gvLayoutJobs(Gvc, G); /* take layout engine from command line */
+			gvFreeLayout(Gvc, G);
+			agclose(G);
+		}
 	}
-    }
-    else if ((G = gvPluginsGraph(Gvc))) {
-	    gvLayoutJobs(Gvc, G);  /* take layout engine from command line */
-	    gvRenderJobs(Gvc, G);
-    }
-    else {
-	while ((G = gvNextInputGraph(Gvc))) {
-	    if (prev) {
-		gvFreeLayout(Gvc, prev);
-		agclose(prev);
-	    }
-	    gvLayoutJobs(Gvc, G);  /* take layout engine from command line */
-	    gvRenderJobs(Gvc, G);
-	    r = agreseterrors();
-	    rc = MAX(rc,r);
-	    prev = G;
+	else if ((G = gvPluginsGraph(Gvc)))
+	{
+		gvLayoutJobs(Gvc, G); /* take layout engine from command line */
+		gvRenderJobs(Gvc, G);
 	}
-    }
-    gvFinalize(Gvc);
-    
-    r = gvFreeContext(Gvc);
-    return (MAX(rc,r));
+	else
+	{
+		while ((G = gvNextInputGraph(Gvc)))
+		{
+			if (prev)
+			{
+				gvFreeLayout(Gvc, prev);
+				agclose(prev);
+			}
+			gvLayoutJobs(Gvc, G); /* take layout engine from command line */
+			gvRenderJobs(Gvc, G);
+			r = agreseterrors();
+			rc = MAX(rc, r);
+			prev = G;
+		}
+	}
+	gvFinalize(Gvc);
+
+	r = gvFreeContext(Gvc);
+	return (MAX(rc, r));
 }
