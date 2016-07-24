9f3f4cada3b068f081852a028b14ee12e6785441
Erwin Janssen <erwinjanssen@outlook.com>
2016-07-24 16:11:56 +0200

    Added gv_stream_and_exit_info in command_line.h
    This struct can be used to pass information about which stream to use for output and error reporting and which function to call when the program needs to exit.
    This struct can be used in the gvParseArgs function to improve testability.

--- lib/common/command_line.h
+++ lib/common/command_line.h
@@ -11,6 +11,21 @@
 #ifndef LIB_COMMON_COMMAND_LINE_H_
 #define LIB_COMMON_COMMAND_LINE_H_
 
+#include <stdio.h>
 
+/**
+ * This struct can be used to pass information about which stream to use for
+ * output and error reporting and which function to call when the program needs
+ * to exit.
+ */
+typedef struct gv_stream_and_exit_info {
+	/** The stream to use for default output and printing, often stdout. */
+	FILE* output_stream;
+	/** The stream to use for error reporting, often stderr. */
+	FILE* error_stream;
+	/** The address of the function to call when requesting the exit of the
+	 *  program, often the address of exit(). */
+	void (*exit_function)(int);
+} gv_stream_and_exit_info;
 
 #endif /* LIB_COMMON_COMMAND_LINE_H_ */
