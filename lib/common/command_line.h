008fd4c82e452131dc1940f8aa2059f293315e00
Erwin Janssen <erwinjanssen@outlook.com>
2016-08-10 12:52:51 +0200

    Added new command_line source and header.
    These empty files will serve as the home for new code related to the command line and the parsing of command line arguments.

---
+++ lib/common/command_line.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at:
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: See version control history
+ * Details at https://github.com/ellson/graphviz
+ *************************************************************************/
+
+#ifndef LIB_COMMON_COMMAND_LINE_H_
+#define LIB_COMMON_COMMAND_LINE_H_
+
+
+
+#endif /* LIB_COMMON_COMMAND_LINE_H_ */
