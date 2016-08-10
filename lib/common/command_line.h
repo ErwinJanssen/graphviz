0dd185a6b295ed5312125cac8e0b8aea7274ac45
Erwin Janssen <erwinjanssen@outlook.com>
2016-08-10 16:10:31 +0200

    Rough beginning for new command line parsing.
    Very rough beginning for a new command line parsing, lays the foundation for parsing command line arguments that do not require an argument.

--- lib/common/command_line.h
+++ lib/common/command_line.h
@@ -12,15 +12,66 @@
 #define LIB_COMMON_COMMAND_LINE_H_
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
 
 /**
  * This struct contains the possible options and values that are passed as
  * arguments on the command line. To initialize this struct with the proper
  * default values set, use initialize_gv_config().
  */
-typedef struct {
+typedef struct
+{
 	bool print_version;
+	bool print_usage;
 } gv_config;
+
+/**
+ * Enum for the different types of flags supported by the command line
+ */
+typedef enum
+{
+	ARGUMENT_WITH_VALUE, ARGUMENT_WITHOUT_VALUE,
+} gv_argument_type;
+
+/**
+ * This struct describes a command line arguments accepted by Graphviz.
+ * The field offset is the position of the variable in the gv_config that is
+ * affected by this argument.
+ */
+typedef struct
+{
+	char flag;
+	gv_argument_type argument_type;
+	char* description;
+	size_t field_offset;
+} gv_cmdline_argument;
+
+/**
+ * A list containing information about the common command line arguments.
+ * Its length is stored in GV_COMMON_ARGUMENTS_LENGTH.
+ */
+gv_cmdline_argument gv_common_arguments[] =
+{
+	{
+		.flag = 'V',
+		.argument_type = ARGUMENT_WITHOUT_VALUE,
+		.description = "Print the version and exit",
+		.field_offset = offsetof(gv_config, print_version)
+	},
+	{
+		.flag = '?',
+		.argument_type = ARGUMENT_WITHOUT_VALUE,
+		.description = "Print the usage and exit",
+		.field_offset = offsetof(gv_config, print_usage)
+	}
+};
+
+/**
+ * The length of the gv_common_arguments array.
+ */
+const size_t GV_COMMON_ARGUMENTS_LENGTH = 2;
 
 /**
  * This function returns an initialized gv_config struct with all values
@@ -36,5 +87,27 @@ gv_config* initialize_gv_config(void);
  */
 void free_gv_config(gv_config** config);
 
+/**
+ * Parses the command line arguments and creates a gv_config struct based on
+ * the contents of the given arguments. This function will not perform any
+ * other action based on the contents of the arguments, this is done by the
+ * `gv_process_arguments` function.
+ */
+gv_config* gv_parse_arguments(int argc, char** argv);
+
+/**
+ * Called by `gv_parse_arguments` to parse the flags that do not have
+ * a separate value.
+ * This function assumes that `flags` starts with a dash and that it is a
+ * null terminated string.
+ */
+void gv_parse_flags_without_value(gv_config* config, char* flags);
+
+/**
+ * Processes a gv_config struct and acts upon its contents. If gv_config has
+ * `print_version` set, the version will be printed and this function will
+ * call exit.
+ */
+void gv_process_arguments(gv_config* config);
 
 #endif /* LIB_COMMON_COMMAND_LINE_H_ */
