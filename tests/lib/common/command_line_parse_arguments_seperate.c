f1a2b4dc989a0441e028cbb965ea921554312850
Erwin Janssen <erwinjanssen@outlook.com>
2016-08-11 16:57:29 +0200

    Split command_line_parse_arguments.c
    This file became cluttered, so it is split into two new files.

---
+++ tests/lib/common/command_line_parse_arguments_seperate.c
@@ -0,0 +1,47 @@
+/*************************************************************************
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at:
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: See version control history
+ * Details at https://github.com/ellson/graphviz
+ *************************************************************************/
+
+#include "command_line.h"
+
+#include <criterion/criterion.h>
+
+static void compare_gv_config(gv_config* actual, gv_config* expected)
+{
+	cr_expect_eq(actual->print_version, expected->print_version);
+	cr_expect_eq(actual->print_usage, expected->print_usage);
+}
+
+Test(command_line_parse_arguments, dash_V)
+{
+	gv_config* expected_config = initialize_gv_config();
+	expected_config->print_version = true;
+
+	int argc = 2;
+	char* argv[] = {"dot", "-V"};
+	gv_config* actual_config = gv_parse_arguments(argc, argv);
+	compare_gv_config(expected_config, actual_config);
+
+	free_gv_config(&expected_config);
+	free_gv_config(&actual_config);
+}
+
+Test(command_line_parse_arguments, dash_questionmark)
+{
+	gv_config* expected_config = initialize_gv_config();
+	expected_config->print_usage = true;
+
+	int argc = 2;
+	char* argv[] = {"dot", "-?"};
+	gv_config* actual_config = gv_parse_arguments(argc, argv);
+	compare_gv_config(expected_config, actual_config);
+
+	free_gv_config(&expected_config);
+	free_gv_config(&actual_config);
+}
