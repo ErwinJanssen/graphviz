5ba7ba3198f89370f330d4a6382aacf312b617da
Erwin Janssen <erwinjanssen@outlook.com>
2016-08-16 12:14:01 +0200

    Added placeholders for flags parsed in input.c
    The gv_common_arguments array now contains placeholders for all the flags that are parsed in `dotneato_args_initialize`.

--- lib/common/command_line_arguments.c
+++ lib/common/command_line_arguments.c
@@ -29,6 +29,72 @@ gv_cmdline_argument gv_common_arguments[] =
 		.argument_type = ARGUMENT_WITHOUT_VALUE,
 		.description = "Reduce graph",
 		.field_offset = offsetof(gv_config, reduce)
+	},
+	{
+		.flag = 'y',
+		.argument_type = ARGUMENT_WITHOUT_VALUE,
+		.description = "Invert y coordinate in output",
+		//.field_offset =
+	},
+	{
+		.flag = 'P',
+		.argument_type = ARGUMENT_WITHOUT_VALUE,
+		.description = "Internally generate a graph of the current plugins",
+		//.field_offset =
+	},
+	{
+		.flag = 'q',
+		.argument_type = ARGUMENT_WITH_OPTIONAL_VALUE,
+		.description = "Set level of message suppression default=1)",
+		//.field_offset =
+	},
+	{
+		.flag = 's',
+		.argument_type = ARGUMENT_WITH_OPTIONAL_VALUE,
+		.description = "Scale input by this value, default=72",
+		//.field_offset =
+	},
+	{
+		.flag = 'G',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Set additional graph attribute in the form of name=val",
+		//.field_offset =
+	},
+	{
+		.flag = 'N',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Set additional node attribute in the form of name=val",
+		//.field_offset =
+	},
+	{
+		.flag = 'E',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Set additional edge attribute in the form of name=val",
+		//.field_offset =
+	},
+	{
+		.flag = 'T',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Set output format",
+		//.field_offset =
+	},
+	{
+		.flag = 'K',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Set layout engine",
+		//.field_offset =
+	},
+	{
+		.flag = 'l',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Use external library",
+		//.field_offset =
+	},
+	{
+		.flag = 'o',
+		.argument_type = ARGUMENT_WITH_MULTIPLE_VALUES,
+		.description = "Specify output file",
+		//.field_offset =
 	}
 };
 
