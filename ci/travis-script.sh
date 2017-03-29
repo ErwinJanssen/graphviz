142b646edca5795a9f6fb6c6493bb909326fef9d
Erwin Janssen <erwinjanssen@outlook.com>
2017-03-29 13:31:16 +0200

    Add SonarQube scan to Travis
    SonarQube is tool that helps to increase code quality. They have an
    online version that can be integrated with Travis.

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd ${TRAVIS_BUILD_DIR}
-if [ ! ${DOCKER_BUILD} ] ; then
+if [ ${SONARQUBE} ] ; then
+    build-wrapper-linux-x86-64 --out-dir bw-output ./ci/${BUILD_SYSTEM}-build_and_test.sh
+    sonar-scanner
+elif [ ! ${DOCKER_BUILD} ] ; then
     ./ci/${BUILD_SYSTEM}-build_and_test.sh
 elif [ ${DOCKER_BUILD} ] ; then
     docker run --rm -v `pwd`:/graphviz -w /graphviz -i -t centos7-build-env bash "ci/${BUILD_SYSTEM}-build_and_test.sh"
